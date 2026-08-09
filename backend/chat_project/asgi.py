8ec083ef29ebbed34f22a640e672f428a185e76b
Orokunle Ayomide <orokunleayomide@Orokunles-MacBook-Air.local>
2026-08-09 23:06:02 +0100

    Fix ASGI Django initialization order

--- backend/chat_project/asgi.py
+++ backend/chat_project/asgi.py
@@ -1,14 +1,14 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from chat.middleware import JWTAuthMiddleware
-from chat.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.middleware import JWTAuthMiddleware
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
